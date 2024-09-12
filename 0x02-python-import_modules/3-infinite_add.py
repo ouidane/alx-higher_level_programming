62ebd07bd76f43b09e3d38036fb68ddf730ef40d
zakaria ouidane <ouidanezakaria@gmail.com>
2024-09-12 14:58:00 -0700

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    result = 0
+
+    for i in range(len(sys.argv) - 1):
+        result += int(sys.argv[i + 1])
+
+    print("{}".format(result))
