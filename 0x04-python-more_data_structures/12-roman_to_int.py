fe5a4ba339db242039b34cf55bc182c6c128585d
ouidane <ouidanezakaria@gmail.com>
2024-09-18 21:09:26 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,8 +4,8 @@ def roman_to_int(roman_string):
         return 0
 
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    total = 0
     prev_value = 0
+    total = 0
 
     for char in reversed(roman_string):
         value = roman_dict.get(char, 0)
