20aa1a660089ef1b566c18551207c58bae3a279d
ouidane <ouidanezakaria@gmail.com>
2024-09-30 17:05:56 +0000

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+def safe_print_list(my_list=[], x=0):
+    printed_elts = 0
     try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
+        for i in range(x):
+            print(my_list[i], end="")
+            printed_elts += 1
+    except IndexError:
+        pass
+
+    print()
+    return printed_elts
