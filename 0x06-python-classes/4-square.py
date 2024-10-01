6cdd2f5823aab5840f78b952a007eae064a6bb4f
ouidane <ouidanezakaria@gmail.com>
2024-10-01 23:52:34 +0100

    update task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,26 +14,21 @@ class Square:
             size (int): The size of the square (default is 0).
         """
         self.size = size  # Use the setter for validation
-    
+
     @property
     def size(self):
         """Retrieves the size of the square."""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
-        """
-        Sets the size of the square with validation.
-
-        Args:
-            value (int): The new size of the square.
-        """
+        """Sets the size of the square with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         """Computes the area of the square."""
         return self.__size ** 2
