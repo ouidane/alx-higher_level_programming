e5c1b814484fb7be600ae7266926a9e988c3cde2
ouidane <ouidanezakaria@gmail.com>
2024-10-08 01:13:02 +0100

    task 10

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -11,10 +11,12 @@ def is_safe(board, row, col, n):
         if board[i][col] == 1:
             return False
 
+    # Check upper-left diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
+    # Check upper-right diagonal
     for i, j in zip(range(row, -1, -1), range(col, n)):
         if board[i][j] == 1:
             return False
