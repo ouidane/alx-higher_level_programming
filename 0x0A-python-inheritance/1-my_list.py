9cd5523e08da5d0e915da58ec41ae493c6869814
ouidane <ouidanezakaria@gmail.com>
2024-10-15 00:01:20 +0100

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a list class"""
+
+class MyList(list):
+    """Subclass of list"""
+    def __init__(self):
+        """Initialize the list"""
+        super().__init__()
+
+    def print_sorted(self):
+        """print a sorted list"""
+        ascended_list = sorted(self)
+        print(ascended_list)
