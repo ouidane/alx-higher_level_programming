dac823228f6268fef8025c05d13a4c83d0cea6cf
ouidane <ouidanezakaria@gmail.com>
2024-10-16 02:11:55 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,18 +3,6 @@
 
 
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints it to stdout
-
-    Args:
-        filename (str): The name of the file
-
-    Returns:
-        None
-    """
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                print(line, end='')
-    except FileNotFoundError:
-        pass
+    """Reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
