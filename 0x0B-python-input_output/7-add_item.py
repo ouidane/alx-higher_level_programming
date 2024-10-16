e465070c78261f1041959dfa398fe581ee35e356
ouidane <ouidanezakaria@gmail.com>
2024-10-16 01:54:09 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,50 +1,17 @@
 #!/usr/bin/python3
 """Defines a save_to_json_file function"""
 
-
 import sys
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    """
-    Save an object to a JSON file.
-
-    Args:
-        my_obj: The object to be saved to the file.
-        filename (str): The name of the JSON file.
-
-    Returns:
-        None
-    """
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
-
-
-def load_from_json_file(filename):
-    """
-    Load an object from a JSON file.
-
-    Args:
-        filename (str): The name of the JSON file.
-
-    Returns:
-        object: The object loaded from the file.
-    """
-    try:
-        with open(filename, 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []
 
 
 if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
     try:
-        existing_data = load_from_json_file("add_item.json")
-    except json.JSONDecodeError:
-        existing_data = []
-
-    for arg in sys.argv[1:]:
-        existing_data.append(arg)
-
-    save_to_json_file(existing_data, "add_item.json")
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
