935af15e315e23079c535ef4ed8ea3a7db1d6fcd
ouidane <ouidanezakaria@gmail.com>
2024-12-17 00:59:37 +0100

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
 function factorial(n) {
-  if (n <= 1) {
-    return 1;
+  if (n === 0 || isNaN(n)) {
+    return (1);
   }
   return n * factorial(n - 1);
 }
 
-const arg = parseInt(process.argv[2]);
-
-console.log(factorial(isNaN(arg) ? 1 : arg));
+console.log(factorial(parseInt(process.argv[2])));
