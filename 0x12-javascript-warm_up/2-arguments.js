762cb448c74a3f2ca8e6c78e8ae67ad8f1662be2
ouidane <ouidanezakaria@gmail.com>
2024-12-17 00:31:43 +0100

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const argCount = process.argv.length - 2;
 
 if (argCount === 0) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (argCount === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
