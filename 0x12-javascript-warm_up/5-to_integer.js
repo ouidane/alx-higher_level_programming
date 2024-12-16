c58a5c61ea9807acae763f718f285761f1d37b8e
ouidane <ouidanezakaria@gmail.com>
2024-12-16 23:38:49 +0100

    task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+const num = parseInt(firstArg);
+
+if (isNaN(num) || firstArg === undefined) {
+  console.log("Not a number");
+} else {
+  console.log(`My number: ${num}`);
+}
