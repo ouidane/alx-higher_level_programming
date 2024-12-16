612c9dbc5c81f8f655f0325d30cd53e1c92cb5f9
ouidane <ouidanezakaria@gmail.com>
2024-12-16 23:38:50 +0100

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+
+if (firstArg === undefined || isNaN(firstArg)) {
+  console.log("Missing number of occurrences");
+} else {
+  const num = parseInt(firstArg);
+  let i = 0;
+  while (i < num) {
+    console.log("C is fun");
+    i++;
+  }
+}
