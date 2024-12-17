7ebf448cf4cfcff41d4f31d9c1dfdbba9fadb938
ouidane <ouidanezakaria@gmail.com>
2024-12-17 01:07:13 +0100

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function factorial(num) {
-  if (num === 0 || isNaN(num)) {
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
     return (1);
   }
-  return num * factorial(num - 1);
+  return (n * factorial(n - 1));
 }
 
 console.log(factorial(Number(process.argv[2])));
