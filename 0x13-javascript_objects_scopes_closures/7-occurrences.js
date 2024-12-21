f74fd02140464d1cf882b9e09b35f430bfb7483a
ouidane <ouidanezakaria@gmail.com>
2024-12-21 01:55:07 +0100

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let element = 0; element < list.length; element++) {
-    if (searchElement === list[element]) {
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
       count++;
     }
   }
