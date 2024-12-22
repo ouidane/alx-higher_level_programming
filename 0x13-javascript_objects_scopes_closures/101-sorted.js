8bef62cf8c45320cf9a31f131dcea55ca40b9efb
ouidane <ouidanezakaria@gmail.com>
2024-12-22 01:26:03 +0100

    task 11

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,13 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
+const dict = require("./101-data").dict;
 
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
 const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
-    }
+
+for (const [userId, occurrences] of Object.entries(dict)) {
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
   }
-  newDict[valsUniq[j]] = list;
+  newDict[occurrences].push(userId);
 }
+
 console.log(newDict);
