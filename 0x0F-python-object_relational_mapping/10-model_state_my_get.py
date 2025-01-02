6546ab87e0446b69f25d2995b509658777afc21e
ouidane <ouidanezakaria@gmail.com>
2025-01-02 01:00:33 +0100

    task 9

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
+"""Print the state of the given `name`."""
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from model_state import State, Base
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
-    try:
-        print(instance[0].id)
-    except IndexError:
+
+    state = session.query(State).filter_by(name=sys.argv[4]).one_or_none()
+    if state is None:
         print("Not found")
+    else:
+        print(state.id)
+
+    session.close()
