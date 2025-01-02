a538e0b410844dc2902836ff514bce8a78702e69
ouidane <ouidanezakaria@gmail.com>
2025-01-02 01:00:32 +0100

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+"""List Cities by States."""
 import sys
-
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    conn = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+    )
+
+    cursor = conn.cursor()
+    cursor.execute(
+        """SELECT cities.id, cities.name, states.name
+        FROM states, cities
+        WHERE cities.state_id = states.id
+        ORDER BY cities.id"""
+    )
+    [print(state) for state in cursor.fetchall()]
+
+    cursor.close()
+    conn.close()
